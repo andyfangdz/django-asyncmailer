cf80ddc8f4ba08bb3f75c25fd4c710f00cebd892
lc4t <lc4t0.0@gmail.com>
2016-07-06 15:30:21 +0800

    Url /asyncmailer/ for admin only

--- asyncmailer/views.py
+++ asyncmailer/views.py
@@ -1,3 +1,14 @@
+from django.http.response import HttpResponse
+from django.http import HttpResponseForbidden
+
+# asyncmailer/ index page
+def index(request):
+    if (request.user.is_authenticated() and request.user.is_active and request.user.is_superuser):
+        return HttpResponse('admin')
+    else:
+        return HttpResponseForbidden()
+
+
 """Views for the asyncmailer app."""
 # from django.views.generic import TemplateView
 
