1b0a3cf2fa90dcc48313fe345f1b5b25f7b333e8
lc4t <lc4t0.0@gmail.com>
2016-07-14 17:24:02 +0800

    PEP8

--- asyncmailer/views.py
+++ asyncmailer/views.py
@@ -2,10 +2,9 @@ from django.http.response import HttpResponse
 from django.contrib.admin.views.decorators import staff_member_required
 from django.shortcuts import render_to_response
 from django.template import RequestContext
-from django.views.decorators.http import require_http_methods
-from asyncmailer.models import  EmailTemplate
 from django.conf import settings
 from django.http import HttpResponseBadRequest
+from asyncmailer.tasks import async_mail
 import re
 import os
 
@@ -13,16 +12,15 @@ import os
 def getOptions():
     try:
         templatepath = settings.ASYNCMAILER_TEMPLATES_PATH
-    except Exception as e:
+    except:
         templatepath = os.path.dirname(__file__) + '/templates/asyncmailer/'
-    # os.path.join(os.path.dirname(__file__) + '/templates/asyncmailer/')
     templates = []
 
     for dirname in os.listdir(templatepath):
         if (not os.path.isdir(templatepath + dirname)):
             continue
         else:
-            newTemplate = {"dir":dirname, "html":"", "variations":[]}
+            newTemplate = {"dir": dirname, "html": "", "variations": []}
             for filename in os.listdir(templatepath + dirname):
                 if (re.findall('\.html$', filename)):
                     newTemplate['html'] = filename
@@ -33,11 +31,17 @@ def getOptions():
             templates.append(newTemplate)
     return templates
 
+from django.views.decorators.csrf import ensure_csrf_cookie
+
 
 @staff_member_required
+@ensure_csrf_cookie
 def index(request):
     templates = [i['html'] for i in getOptions()]
-    return render_to_response('asyncmailer/index.html',{'templates':templates}, context_instance = RequestContext(request))
+    return render_to_response(
+        'asyncmailer/index.html',
+        {'templates': templates})
+
 
 @staff_member_required
 def getVariations(request):
@@ -48,8 +52,9 @@ def getVariations(request):
                 variations = i['variations']
                 break
         return HttpResponse(' '.join(variations))
-    except Exception as e:
+    except:
         return HttpResponseBadRequest()
+
 
 def getform(request):
     try:
@@ -64,42 +69,68 @@ def getform(request):
         else:   # jsons
             payload = {}
         return (template, variation, locale, inline, formats, payload)
-    except Exception as e:
+    except:
         return None
+
 
 @staff_member_required
 def retrieve(request):
     try:
-        template, variation, locale, inline, formats, payload = getform(request)
-        return render_to_response('asyncmailer/' + template.replace('.html', '-templates/') + template, payload, context_instance = RequestContext(request))
+        template, variation, locale, inline, formats, payload = getform(
+            request)
+        return render_to_response(
+            'asyncmailer/' +
+            template.replace(
+                '.html',
+                '-templates/') +
+            template,
+            payload,
+            context_instance=RequestContext(request))
     except Exception as e:
+        print(e)
         return HttpResponseBadRequest()
 
+
 def getDictValue(var, key):
-    if (var.has_key(key)):
+    if (key in var):
         return var[key]
     else:
         return '<NULL>'
 
-@staff_member_required
-def presend(request):
-    # try:
-    template, variation, locale, inline, formats, payload = getform(request)
-    email = request.POST.get('email', '')
 
-    from asyncmailer.tasks import async_mail
-    async_mail([email],
-      "New Activity on Your Account",
-      context_dict={
-      "message": "Hello! \n Someone gave you a comment today! \n \"%s\"" % getDictValue(payload, 'message'),
-      "button_text": getDictValue(payload, 'button_text'),
-      "button_url": "http://www.example.com/login/?token=%s&next=/notifications/" % getDictValue(payload, 'button_url')
-      },
-      template='asyncmailer/' + template.replace('.html', '-templates/') + template,
-    )
-    return render_to_response('asyncmailer/index.html', payload, context_instance = RequestContext(request))
-    # except Exception as e:
-    #     return HttpResponseBadRequest()
+@staff_member_required
+@ensure_csrf_cookie
+def presend(request):
+    try:
+        template, variation, locale, inline, formats, payload = getform(
+            request)
+        email = request.POST.get('email', '')
+        print(email)
+        async_mail(
+            [email],
+            "New Activity on Your Account",
+            context_dict={
+                "message": "Hello! \n Someone gave you a comment today! \n \"%s\"" %
+                getDictValue(
+                    payload,
+                    'message'),
+                "button_text": getDictValue(
+                    payload,
+                    'button_text'),
+                "button_url": "http://?token=%s&next=/notifications/" %
+                getDictValue(
+                    payload,
+                    'button_url')},
+            template='asyncmailer/' +
+            template.replace(
+                '.html',
+                '-templates/') +
+            template)
+        return render_to_response('asyncmailer/index.html',
+                                  payload)
+    except Exception as e:
+        print(e)
+        return HttpResponseBadRequest()
 
 """Views for the asyncmailer app."""
 # from django.views.generic import TemplateView
